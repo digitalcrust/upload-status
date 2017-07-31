d92185d759f1d6f5a3c9c0be149b4e4e537fd2cd
Jeffrey Allen <allenj@usgs.gov>
2017-07-31 14:57:48 -0600

    Add file name when using upload button

--- src/main/app/javascript/components/SBItemFile.js
+++ src/main/app/javascript/components/SBItemFile.js
@@ -3,10 +3,10 @@ import { formatBytes } from '../numberFormatting'
 import { Button } from 'react-bootstrap'
 
 export default class SBItemFile extends Component {
-  movefile(fileurl) {
-    console.log(fileurl)
+  movefile(fileurl, filename) {
+    console.log(fileurl, filename)
     document.getElementById('transferstatus').innerHTML = ''
-    this.props.stomper.send("/app/pull-link",JSON.stringify({'link': fileurl}));
+    this.props.stomper.send("/app/pull-link",JSON.stringify({'link': fileurl, 'fileName': filename}));
   }
   render () {
     var stompSocketClient;
@@ -14,7 +14,7 @@ export default class SBItemFile extends Component {
     const fs = formatBytes(sbfile.size)
     return (<section className='files'>
         <span data-fileurl={sbfile.name} className='movefile'>
-          <Button data-toggle="popover" title="Copy File to S3" onClick={()=>{this.movefile(sbfile.url)}}><span className='glyphicon glyphicon-upload pull-left pull-up' /></Button>
+          <Button data-toggle="popover" title="Copy File to S3" onClick={()=>{this.movefile(sbfile.url, sbfile.name)}}><span className='glyphicon glyphicon-upload pull-left pull-up' /></Button>
         </span>
         <a href={sbfile.url} className='filelink'>{sbfile.name}</a>
         <span className='filesize'>{fs}</span>
